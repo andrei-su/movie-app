db6dab05060be4328204c177e2d789006ed30d7d
andrei <git1@email.com>
2021-09-05 13:44:50 +0200

    added login functionality

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Components
 import Header from './components/Header';
 import Home from './components/Home';
+import Login from './components/Login';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
+// Context
+import UserProvider from './context'
 // Styles
-import {GlobalStyle} from './GlobalStyle';
+import { GlobalStyle } from './GlobalStyle';
 
 const App = () => (
-    <Router>
+  <Router>
+    <UserProvider>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/:movieId' element={<Movie />}/>
-        <Route path='/*' element={<NotFound />}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/:movieId' element={<Movie />} />
+        <Route path='/*' element={<NotFound />} />
       </Routes>
       <GlobalStyle />
-    </Router>
+    </UserProvider>
+  </Router>
 )
 
 export default App;
