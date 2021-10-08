6609f311079110fae3110270c2e55d8e16786de6
andrei <email@a>
2021-10-08 09:46:28 +0200

    Updated Rate styles

---
+++ src/components/Rate/Rate.styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    p {
+        width: fit-content;
+    }
+    p > button {
+        border: 1px solid var(--lightGrey);
+        min-width: 90px;
+        height: 40px;
+        font-size: 20px;
+    }
+    p > button:hover {
+        background: var(--white);
+        color: var(--darkGrey);
+    }
+`;
+
+export const Slider = styled.div`
+    display: flex;
+    
+    .rate__value {
+        margin-left: 10px;
+        font-weight: bold;
+        font-size: 20px;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+`;
